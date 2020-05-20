b55b2d0ca4f0df53bb6396ac10871ad3baf44f6f
David Juarez <djuar05@gmail.com>
2020-05-20 11:40:52 -0400

    add notes of what to complete and what i learned

--- Department.java
+++ Department.java
@@ -21,6 +21,11 @@ public abstract class Department {
         for(Department d : depts){
             // Print out name of Dept and return if that Dept is open today
             System.out.println(d.toString() + " is open today?" + d.isOpenToday(today));
+            //Use instanceof keyword to test if item in List is
+            //of a certain 
+            if(d instanceof ServiceDepartment) {
+                System.out.println(d.toString() + " only closes on Sundays.");
+            }
         }
     }
     public abstract boolean isOpenToday(LocalDateTime day);
