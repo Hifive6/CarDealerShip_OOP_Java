81949a5373c6c4b9be556fb005c2ad431842195a
David Juarez <djuar05@gmail.com>
2020-05-20 11:14:09 -0400

    create Dealership class with empty main method

---
+++ DealerShip.java
@@ -0,0 +1,5 @@
+public class DealerShip {
+    public static void main(String[] args){
+        
+    }
+}
--- VehicleClassification.java
+++ VehicleClassification.java
@@ -2,5 +2,6 @@
     //Our own custom enumeration
 public enum VehicleClassification{
     //Convention to name enum option in all UPPER CASE
-    NEW, USED, OFF_LEASE, SHUTTLE, LOANER, PARTS_RUNNER
+    NEW, USED, OFF_LEASE, SHUTTLE, LOANER, PARTS_RUNNER;
+
 }
