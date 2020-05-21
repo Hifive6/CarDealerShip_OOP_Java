5ef5f9c375e223ea9ba9abd0e3110a6e0decf602
David Juarez <djuar05@gmail.com>
2020-05-21 10:25:47 -0400

    create department factory enum for departments

--- DealerShip.java
+++ DealerShip.java
@@ -4,18 +4,18 @@ import java.util.List;
 
 public class DealerShip {
     public static void main(String[] args){
-        // Instanciate new object for eaach Department
-        ServiceDepartment sd = new ServiceDepartment();
-        FinanceDepartment fd = new FinanceDepartment();
-        SalesDepartment sales = new SalesDepartment();
+        // // Instanciate new object for eaach Department
+        // ServiceDepartment sd = new ServiceDepartment();
+        // FinanceDepartment fd = new FinanceDepartment();
+        // SalesDepartment sales = new SalesDepartment();
 
-        //Add Department to a list
-        List<Department> depts = new ArrayList<>();
-        depts.add(sd);
-        depts.add(fd);
-        depts.add(sales);
+        // //Add Department to a list
+        // List<Department> depts = new ArrayList<>();
+        // depts.add(sd);
+        // depts.add(fd);
+        // depts.add(sales);
 
-        Department.printIsOpen(depts, LocalDateTime.now());
+        // Department.printIsOpen(depts, LocalDateTime.now());
 
 
 
--- OOP Day 3.md
+++ OOP Day 3.md
@@ -14,3 +14,20 @@
 * A Dealership also "has" Vehicles, but ist a less tightly coupled relationship
 * Vehicles come ang o tthroug the Dealerhip and the Deaership can manipulate them with it "own" them
 * But, ultimately, a Vehicle _can_ exist outside of a Dealership - this is called **aggregation**
+
+
+### Generics
+*allows us to create classses and methods that can be used on multiple kinds of data types, elements, etc...
+* <E> for elements, <T> for data type, <K> for key, <V> for value
+* we can 'bound' our generics to specific user-generate object/type by using the 'extends'  keyword
+* example: <E extends Vehicle>
+
+### OOP Design Patterns
+* Sounds really intimidating, but it is not
+* A convention so wll recognized that it hava a name and when someon calls a pattern by its name, everyone knows what they're taling about
+
+
+### Factory pattern
+* a method for creating objects without exposing the logice needed for creation
+* Super common patter in OOP
+* using a Factory insulates the consumer rom the actual class implementation
