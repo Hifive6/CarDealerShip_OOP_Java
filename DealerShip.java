079fa77bb8e080a69756d4aed6299a67527e9d51
David Juarez <djuar05@gmail.com>
2020-05-21 10:07:54 -0400

    create generic class pendingsale

--- DealerShip.java
+++ DealerShip.java
@@ -4,29 +4,34 @@ import java.util.List;
 
 public class DealerShip {
     public static void main(String[] args){
-        // // Instanciate new object for eaach Department
-        // ServiceDepartment sd = new ServiceDepartment();
-        // FinanceDepartment fd = new FinanceDepartment();
-        // SalesDepartment sales = new SalesDepartment();
+        // Instanciate new object for eaach Department
+        ServiceDepartment sd = new ServiceDepartment();
+        FinanceDepartment fd = new FinanceDepartment();
+        SalesDepartment sales = new SalesDepartment();
 
-        // //Add Department to a list
-        // List<Department> depts = new ArrayList<>();
-        // depts.add(sd);
-        // depts.add(fd);
-        // depts.add(sales);
+        //Add Department to a list
+        List<Department> depts = new ArrayList<>();
+        depts.add(sd);
+        depts.add(fd);
+        depts.add(sales);
 
-        // Department.printIsOpen(depts, LocalDateTime.now());
+        Department.printIsOpen(depts, LocalDateTime.now());
 
+
+
+
+
+        //Starting with light Truck and Car classes
         // Leaseable lease = new Car("232342234FDE", 330000.00, 420000.00, 2019, "Chevy", "Impala", "White", VehicleClassification.NEW);
 
         // System.out.println("Number of months to lease " + lease.getLeaseTerm());
 
-        LightTruck lt = new LightTruck("234Jfke123", 48000.00, 62000.00, 2019, "Chevy", "Silverardo", "Silver", 100000L, 900000L, true, VehicleClassification.OFF_LEASE);
+        // LightTruck lt = new LightTruck("234Jfke123", 48000.00, 62000.00, 2019, "Chevy", "Silverardo", "Silver", 100000L, 900000L, true, VehicleClassification.OFF_LEASE);
 
-        if(lt.getIs4wd()){
-            int gears = lt.xferCase.getNumGears();
-            System.out.println("Number of Gears: " + gears);
-        }
-        lt.printVehicleType();
+        // if(lt.getIs4wd()){
+        //     int gears = lt.xferCase.getNumGears();
+        //     System.out.println("Number of Gears: " + gears);
+        // }
+        // lt.printVehicleType();
     }
 }
